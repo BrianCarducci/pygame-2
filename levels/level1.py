6c1cd2f9fee708b9dca0b627ebcd29519601bf1a
Brian Carducci <briancarducci@gmail.com>
2020-08-30 19:13:11 -0400

    starting to add enemies

--- levels/level1.py
+++ levels/level1.py
@@ -2,26 +2,32 @@ import pygame
 import os
 
 from sprites.environment.platform import Platform
+from sprites.enemies.zombie import Zombie
 from sprites.player.player import Player
 
 def setup(window_width, window_height):
-    environment_sprite_group = pygame.sprite.Group()
+    sprite_group = pygame.sprite.Group()
 
     floor_image = pygame.image.load(os.path.join("assets", "sprites", "environment", "floors", "stone_floor.jpg"))
+    zombie_image = pygame.image.load(os.path.join("assets", "sprites", "enemies", "zombie.jpg"))
+
     base_floor_y_loc = window_height - floor_image.get_rect().h
     floor_x_loc = 0
     for i in range(20):
-        environment_sprite_group.add(Platform(floor_image, floor_x_loc, base_floor_y_loc, 0))
+        sprite_group.add(Platform(floor_image, floor_x_loc, base_floor_y_loc, 0))
+        if i == 8:
+            sprite_group.add(Zombie(zombie_image, floor_x_loc, base_floor_y_loc - zombie_image.get_rect().h, 0))
         if i == 15:
-            environment_sprite_group.add(Platform(floor_image, floor_x_loc, base_floor_y_loc - floor_image.get_rect().h, 0))
+            sprite_group.add(Platform(floor_image, floor_x_loc, base_floor_y_loc - floor_image.get_rect().h, 0))
         if i == 10:
-            environment_sprite_group.add(Platform(floor_image, floor_x_loc, base_floor_y_loc - floor_image.get_rect().h * 3, 0))
+            sprite_group.add(Platform(floor_image, floor_x_loc, base_floor_y_loc - floor_image.get_rect().h * 3, 0))
 
         floor_x_loc += floor_image.get_rect().w
+
 
     
 
     player_image = pygame.image.load("assets/sprites/player/123.jpg")
-    player = Player(player_image, window_width/2, 300, 30, False, 30, 30, False)
+    player = Player(player_image, window_width/2, base_floor_y_loc - player_image.get_rect().h, 30, False, 30, 30, False)
 
-    return player, environment_sprite_group
+    return player, sprite_group
--- sprites/enemies/zombie.py
+++ sprites/enemies/zombie.py
@@ -1,17 +1,17 @@
 import pygame
 
-class Platform(pygame.sprite.Sprite):
+class Zombie(pygame.sprite.Sprite):
+
     def __init__(self, image, x_loc, y_loc, vel):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
-        self.rect = image.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.x = x_loc
         self.rect.y = y_loc
         self.vel = vel
 
     def update(self, window, player_actions, player_vel):
         mover = 0
-
         if player_actions:
             if "right" in player_actions:
                 mover = player_vel * -1
